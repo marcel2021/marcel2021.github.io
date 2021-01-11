3ac5d7dd4495ff395e117c7d78b06f0748ca9c80
moonsoo101 <moonsoo101@gmail.com>
2021-01-11 23:28:08 +0900

    gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  const setColumn = () => {
+    if (window.innerWidth > 1200)
+      return 3
+    else if (window.innerWidth > 650)
+      return 2;
+     else
+      return 1;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Gallery columns={setColumn} photos={photos} onClick={openLightbox} direction="column"/>
+    <ModalGateway>
+      {viewerIsOpen ? (
+        <Modal onClose={closeLightbox}>
+          <Carousel
+            currentIndex={currentImage}
+            views={photos.map(x => ({
+              ...x,
+              srcset: x.srcSet,
+              caption: x.title
+            }))}
+          />
+        </Modal>
+      ) : null}
+    </ModalGateway>
+  </div>
   );
 }
 
